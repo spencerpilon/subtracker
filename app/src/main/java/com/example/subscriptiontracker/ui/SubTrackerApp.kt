a7ad7152daa556ce0125b313eef2942348dc3a96
Spencer <spencerpilon@gmail.com>
2024-08-22 17:26:38 -0600

    Initial commit: blank jetpack compose project

---
+++ app/src/main/java/com/example/subscriptiontracker/ui/SubTrackerApp.kt
@@ -0,0 +1,14 @@
+package com.example.subscriptiontracker.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.subscriptiontracker.ui.theme.SubscriptionTrackerTheme
+
+@Preview
+@Composable
+fun SubTrackerApp() {
+    SubscriptionTrackerTheme {
+        Text("Subscription Tracker")
+    }
+}
